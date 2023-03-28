404054ebd5b79080909cf8c4a61413ceb6f02ac7
Ollie Lloyd <olloyd321@gmail.com>
2023-03-28 18:50:18 -0400

    added model code for products, still need to add tag and product tag

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,50 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { INTEGER } = require('sequelize');
+const { Model, DataTypes, INET } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validate syntax right???
+      validate: {
+        isDecimal: true,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          max: 10,
+          isInt: true,
+        }
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Category',
+          key: 'id',
+        },
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
