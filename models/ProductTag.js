bdc1597f49316835135a2b96299fe43f8e08f0ad
Ollie Lloyd <olloyd321@gmail.com>
2023-03-30 17:45:11 -0400

    added code for product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,3 +1,4 @@
+const { INTEGER } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
@@ -6,7 +7,19 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
